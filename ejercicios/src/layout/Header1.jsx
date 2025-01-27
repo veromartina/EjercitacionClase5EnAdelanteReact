49ba089a0204f69848d0a88006e4266860f8115e
Veronica <veromartina8@gmail.com>
2025-01-26 23:01:03 -0300

    se agregan comentarios

--- ejercicios/src/layout/Header1.jsx
+++ ejercicios/src/layout/Header1.jsx
@@ -11,6 +11,13 @@ export const Header1 = () => {
       <nav>
         {/* <Link to='/' >Home</Link>
       <Link to='/login' >Login</Link> */}
+
+{/* 1)NavLink: Es un componente proporcionado por React Router que funciona como un enlace de navegación 
+2)className={({ isActive }) => (isActive ? "active" : "")}: Esta es una función que asigna una clase CSS al enlace. La función recibe un objeto que contiene la propiedad isActive, que es un booleano que indica si la ruta del NavLink es la que está actualmente activa (es decir, si la ruta coincide con la URL del navegador).
+Si isActive es true, se le asigna la clase "active".
+Si isActive es false, no se asigna ninguna clase.
+3)to="/": La propiedad to define a qué ruta se dirigirá el enlace cuando el usuario haga clic. En este caso, el enlace llevará al usuario a la raíz de la aplicación (/).*/}
+
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
           to="/"
