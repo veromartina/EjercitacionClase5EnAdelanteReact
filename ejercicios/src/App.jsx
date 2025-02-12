8cbfb53b5e1d4b849f20feeb93b4de6c045f011c
Veronica <veromartina8@gmail.com>
2025-02-12 14:01:41 -0300

    se agrega archivo con rutas , se modifica app.jsx

--- ejercicios/src/App.jsx
+++ ejercicios/src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import PostPage from './pages/PostPage';  // Página para mostrar todas las publicaciones
-import CreatePage from './pages/CreatePage';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './componets/NavBar';
-import PostDetail from './componets/PostDetail';  // Página para ver el detalle de una publicación
+import AppRoutes from './Routes'; // Importa las rutas desde el archivo Routes.jsx
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostPage />} /> {/* Aquí mostramos la lista de publicaciones */}
-        <Route path="/post/:id" element={<PostDetail />} /> {/* Detalles de una publicación */}
-        <Route path="/create" element={<CreatePage />} />
-      </Routes>
+      <AppRoutes /> {/* Aquí se usan las rutas definidas en Routes.jsx */}
     </Router>
   );
 }
--- ejercicios/src/Routes.jsx
+++ ejercicios/src/Routes.jsx
@@ -1,38 +1,20 @@
 import React from "react";
 import { Route, Routes as RoutesReact } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login"
-//import Products from "./pages/Products";
+import { Routes} from 'react-router-dom';
+import PostPage from './pages/PostPage';
+import CreatePage from './pages/CreatePage';
+import PostDetail from './componets/PostDetail';
 
-import NotFound from "./pages/Notfound";
-import MockapiFetch from "./pages/MockapiFetch";
-//import ProductDetails from "./componets/ProductDetails";
-import MockapiDetails from "./pages/MockapiDetails";
-import Account from "./pages/Account";
-import ProtectedRoute from "./componets/ProtectedRoute";
-
-const Routes = () => {
+function AppRoutes() {
   return (
-    <RoutesReact>
+    <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="login" element={<Login />} />
-      {/*} <Route path="/products" element={<Products />} />
-      <Route path="/products/:id" element={<ProductDetails/>}/> */}
-      <Route path="*" element={<NotFound />} />
-      <Route path="/mockapi" element={<MockapiFetch />} />
-      <Route path="/mockapi/:id" element={<MockapiDetails />} />
-
-      <Route element={<ProtectedRoute />}>   {/* rutas protegidas, encierro las rutas que quiero como  protegidas */}
-        <Route path="/account" element={<Account />}>
-          {/* representan componentes hijos , van sin la barra, sino comenzarian como el home  */}
-          <Route path="cart" element={<div>Carrito</div>} />{" "}
-          {/*en lugar de div uso componentes */}
-          <Route path="profile" element={<div>Perfil</div>} />
-        </Route>
-      </Route>
-      {/*lleva etiqueta de apertura y cierre diferentes a las otras rutas, me da la posibilidad de poner un children (mostrar un contenido hijo). en este caso va a renderizar losdos hijos: carrito y perfil*/}
-    </RoutesReact>
+      <Route path="/posts" element={<PostPage />} />
+      <Route path="/post/:id" element={<PostDetail />} />
+      <Route path="/create" element={<CreatePage />} />
+    </Routes>
   );
-};
+}
 
-export default Routes;
+export default AppRoutes;
